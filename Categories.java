db37c40ec33e74f39f1eb74ead82768e35564d54
Luis <luiscarlos2106@hotmail.com>
2025-10-17 16:53:31 -0600

    Desafío 1

--- Categories.java
+++ Categories.java
@@ -1,3 +1,64 @@
-public class Desafio {
-    
+// Importaciones requeridas
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class Categories {
+
+    // Clase de categoria con la estructura de id, nombre y subcategorias 
+    public static class Category {
+        public final int id;
+        public final String name;
+        public final List<Category> subcategories;
+
+        public Category(int id, String name, List<Category> subcategories) {
+            this.id = id;
+            this.name = name;
+            this.subcategories = subcategories;
+        }
+
+    }
+
+    // Clase auxiliar que define estructura para el nodo y la ruta acumulada
+    public static class NodePath {
+        final Category node; // Categoria actual
+        final String path;   // Ruta hasta ese nodo
+
+        NodePath(Category node, String path) {
+            this.node = node;
+            this.path = path;
+        }
+    }
+
+    // Desafío 1
+    public static List<String> getCategoryPaths(Category root){
+        List<String> result = new ArrayList<>();
+        
+        // Caso si es nula la categoria
+        if(root == null) return result;
+
+        //Ruta inicial
+        Deque<NodePath> stack = new ArrayDeque<>();
+        stack.push(new NodePath(root, root.name));
+
+        while(!stack.isEmpty()){
+            // Obtener el node actual
+            NodePath np = stack.pop();
+            Category node = np.node;
+
+            // Si no tiene subcategorias añadir ruta completa al resultado
+            if (node.subcategories == null || node.subcategories.isEmpty()) {
+                result.add(np.path);
+            } else {
+                // Si tiene subcategorías recorrer los hijos al reves
+                for (int i = node.subcategories.size() - 1; i >= 0; i--){
+                    Category child = node.subcategories.get(i);
+                    stack.push(new NodePath(child,np.path + "/" + child.name));
+                }
+            }
+        }
+        return result;
+    }
+
 }
